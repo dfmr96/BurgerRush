3dcdcbf3ba3309c5d2965d72fbcff0a249ed9c4b
David Marcano <dfmr96@gmail.com>
2025-05-18 03:05:08 -0300

    feat(ads): Banner ad runs on MainMenu

--- Assets/LevelPlayAds.cs
+++ Assets/LevelPlayAds.cs
@@ -1,49 +1,45 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Services.Ads;
 using UnityEngine;
-using com.unity3d.mediation;
+using Unity.Services.LevelPlay;
+using Mediation = com.unity3d.mediation;
+
 
 public class LevelPlayAds : MonoBehaviour
 {
-    private string adUnitId = "2ysgx5mehskfdlh3";
-    private LevelPlayBannerAd bannerAd;
+    private string appKey = "220ee44fd";
+    private BannerAdSample _bannerAd;
 
     private void Start()
     {
-// Init the SDK when implementing the Multiple Ad Units API for Interstitial and Banner formats, with Rewarded using legacy APIs 
-        //LevelPlayAdFormat[] legacyAdFormats = new[] { LevelPlayAdFormat.REWARDED, LevelPlayAdFormat.BANNER, LevelPlayAdFormat.INTERSTITIAL};
-        IronSource.Agent.setMetaData("is_test_suite", "enable");
-        IronSource.Agent.validateIntegration();
-        
-        //LevelPlay.Init("220ee44fd");
-        IronSource.Agent.init("220ee44fd");
+        //IronSource.Agent.init(appKey);
+        LevelPlay.Init(appKey);
     }
 
     private void OnEnable()
     {
+        //IronSourceEvents.onSdkInitializationCompletedEvent += SdkInitializationCompletedEvent;
         LevelPlay.OnInitSuccess += SdkInitializationCompletedEvent;
         LevelPlay.OnInitFailed += SdkInitializationFailedEvent;
-        IronSourceEvents.onSdkInitializationCompletedEvent += SdkInitializationCompletedEvent;
-
-// Implement the events
-
     }
 
     private void SdkInitializationCompletedEvent()
     {
-        IronSource.Agent.launchTestSuite();
+        Debug.Log("SDK Init completed");
     }
 
     private void SdkInitializationFailedEvent(LevelPlayInitError obj)
     {
-        //throw new NotImplementedException();
+        Debug.Log("SDK Init failed");
     }
 
     private void SdkInitializationCompletedEvent(LevelPlayConfiguration obj)
     {
-        Debug.Log("LevelPlay SDK initialized successfully. Calling Test Suite");
-        IronSource.Agent.launchTestSuite();
+        Debug.Log("SDK Init completed");
+        LoadBanner();
+        Debug.Log("Load Banner called");
     }
 
     void OnApplicationPause(bool isPaused) 
@@ -54,30 +50,13 @@ public class LevelPlayAds : MonoBehaviour
 
     public void LoadBanner()
     {
-        bannerAd = new LevelPlayBannerAd(adUnitId, LevelPlayAdSize.BANNER
-            , LevelPlayBannerPosition.BottomCenter);
-        // Register to the events 
-        bannerAd.OnAdLoaded += BannerOnAdLoadedEvent;
-        bannerAd.OnAdLoadFailed += BannerOnAdLoadFailedEvent;
-        bannerAd.OnAdDisplayed += BannerOnAdDisplayedEvent;
-        bannerAd.OnAdDisplayFailed += BannerOnAdDisplayFailedEvent;
-        bannerAd.OnAdClicked += BannerOnAdClickedEvent;
-        bannerAd.OnAdCollapsed += BannerOnAdCollapsedEvent;
-        bannerAd.OnAdLeftApplication += BannerOnAdLeftApplicationEvent;
-        bannerAd.OnAdExpanded += BannerOnAdExpandedEvent;
+        _bannerAd = new BannerAdSample();
+        Debug.Log("Load Banner clicked");
     }
 
     public void DestroyBanner()
     {
-        bannerAd.DestroyAd();
+        _bannerAd.DestroyBannerAd();
+        Debug.Log("Banner destroyed");
     }
-    // BANNER EVENTS
-       private void BannerOnAdLoadedEvent(LevelPlayAdInfo adInfo) {}
-       private void BannerOnAdLoadFailedEvent(LevelPlayAdError ironSourceError) {}
-       private void BannerOnAdClickedEvent(LevelPlayAdInfo adInfo) {}
-       private void BannerOnAdDisplayedEvent(LevelPlayAdInfo adInfo) {}
-       private void BannerOnAdDisplayFailedEvent(LevelPlayAdDisplayInfoError adInfoError) {}
-       private void BannerOnAdCollapsedEvent(LevelPlayAdInfo adInfo) {}
-       private void BannerOnAdLeftApplicationEvent(LevelPlayAdInfo adInfo) {}
-       private void BannerOnAdExpandedEvent(LevelPlayAdInfo adInfo) {}
 }
---
+++ Assets/Scripts/Services/Ads/BannerAdSample.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Unity.Services.LevelPlay;
+using UnityEngine;
+
+namespace Services.Ads
+{
+    public class BannerAdSample
+    {
+        private LevelPlayBannerAd bannerAd;
+
+        public BannerAdSample()
+        {
+            CreateBannerAd();
+        }
+
+        private void CreateBannerAd()
+        {
+            //Create banner instance
+            var size = com.unity3d.mediation.LevelPlayAdSize.CreateAdaptiveAdSize();
+            int width = size.Width;
+            int height = size.Height;
+            var position = com.unity3d.mediation.LevelPlayBannerPosition.BottomCenter;
+
+            bannerAd = new LevelPlayBannerAd("2ysgx5mehskfdlh3", size, position);
+            
+            Debug.Log("Banner created");
+            //Subscribe BannerAd events
+            bannerAd.OnAdLoaded += BannerOnAdLoadedEvent;
+            bannerAd.OnAdLoadFailed += BannerOnAdLoadFailedEvent;
+            bannerAd.OnAdDisplayed += BannerOnAdDisplayedEvent;
+            bannerAd.OnAdDisplayFailed += BannerOnAdDisplayFailedEvent;
+            bannerAd.OnAdClicked += BannerOnAdClickedEvent;
+            bannerAd.OnAdCollapsed += BannerOnAdCollapsedEvent;
+            bannerAd.OnAdLeftApplication += BannerOnAdLeftApplicationEvent;
+            bannerAd.OnAdExpanded += BannerOnAdExpandedEvent;
+            Debug.Log("Events subscribed");
+            //Load the banner ad
+            LoadBannerAd();
+        }
+
+        private void LoadBannerAd()
+        {
+            //Load the banner ad 
+            bannerAd.LoadAd();
+        }
+
+        void ShowBannerAd()
+        {
+            //Show the banner ad, call this method only if you turned off the auto show when you created this banner instance.
+            bannerAd.ShowAd();
+        }
+
+        void HideBannerAd()
+        {
+            //Hide banner
+            bannerAd.HideAd();
+        }
+
+        public void DestroyBannerAd()
+        {
+            //Destroy banner
+            bannerAd.DestroyAd();
+        }
+
+        void BannerOnAdLoadedEvent(LevelPlayAdInfo adInfo)
+        {
+            Debug.Log("Banner loaded successfully");
+        }
+
+        void BannerOnAdLoadFailedEvent(LevelPlayAdError ironSourceError)
+        {
+            Debug.Log("Banner load failed: " + ironSourceError);
+        }
+
+        void BannerOnAdClickedEvent(LevelPlayAdInfo adInfo)
+        {
+            Debug.Log("Banner clicked");
+        }
+
+        void BannerOnAdDisplayedEvent(LevelPlayAdInfo adInfo)
+        {
+            Debug.Log("Banner displayed");
+        }
+
+        void BannerOnAdDisplayFailedEvent(LevelPlayAdDisplayInfoError adInfoError)
+        {
+            Debug.Log("Banner display failed: " + adInfoError);
+        }
+
+
+        void BannerOnAdCollapsedEvent(LevelPlayAdInfo adInfo)
+        {
+            Debug.Log("Banner collapsed");
+        }
+
+        void BannerOnAdLeftApplicationEvent(LevelPlayAdInfo adInfo)
+        {
+            Debug.Log("Banner left application");
+        }
+
+        void BannerOnAdExpandedEvent(LevelPlayAdInfo adInfo)
+        {
+            Debug.Log("Banner expanded");
+        }
+    }
+}
